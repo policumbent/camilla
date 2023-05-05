c4e2ebc8e08543a9fc5cbf98e006415d46889f42
Federico Morro <fedemorro01@gmail.com>
2023-05-05 18:38:33 +0200

    added linear potentiometer reading in move in HR4988

--- lib/HR4988/HR4988.cpp
+++ lib/HR4988/HR4988.cpp
@@ -81,20 +81,21 @@ void HR4988 :: setup () {
 void HR4988 :: move(int start_pos, int target_pos,
                     AS5600 &rotative_encoder, Potentiometer &linear_potentiometer, uint8_t *limit_reached) {
     long int elapsed_time, delay;
-    int delta_angle;
+    int step_cnt;
+    uint16_t delta_angle, delta_linear;
     
     #if DEBUG_HR4988
         Serial.print("Shift from "); Serial.print(start_pos); Serial.print(" to "); Serial.println(target_pos);
         long int debug_t = micros();
-        int cnt = 0, expected_delay = 0, tot_angle = 0;
+        int cnt = 0, expected_delay = 0, tot_angle = 0, tot_linear = 0;
     #endif
     
+    step_cnt = 0;
+
     while (position_sixteenth != target_pos && !(*limit_reached)) {
 
         portDISABLE_INTERRUPTS();
         elapsed_time = micros();
-
-        delta_angle = rotative_encoder.get_angle();
 
         _move_set_speed_direction(start_pos, target_pos);
 
@@ -102,7 +103,14 @@ void HR4988 :: move(int start_pos, int target_pos,
         delayMicroseconds(delay_on);
         digitalWrite(step_pin, LOW);
 
-        delta_angle = rotative_encoder.read_angle() - delta_angle;
+        if (step_cnt % 2 == 0) {
+            delta_angle = rotative_encoder.get_angle();
+            delta_angle = rotative_encoder.read_angle() - delta_angle;
+        }
+        else if (step_cnt % 5 == 0) {
+            delta_linear = linear_potentiometer.get_position();
+            delta_linear = linear_potentiometer.read_position() - delta_linear;
+        }
 
         elapsed_time = micros() - elapsed_time;
         delay = (delay_off - elapsed_time > 0) ? (delay_off - elapsed_time) : (1);
@@ -111,14 +119,16 @@ void HR4988 :: move(int start_pos, int target_pos,
         delayMicroseconds(delay);
 
         // TODO: include position correction
-        // TODO: reading potentiometer, when?
 
         _update_position();
+
+        step_cnt++;
 
         #if DEBUG_HR4988
             cnt++;
             expected_delay += get_expected_step_time();
             tot_angle += delta_angle;
+            tot_linear += delta_linear;
         #endif
     }
 
@@ -127,7 +137,8 @@ void HR4988 :: move(int start_pos, int target_pos,
         if (cnt == 0) return;
         Serial.print("Expected (avg) delay: "); Serial.print(expected_delay / cnt);
         Serial.print("\tMeasured (avg) delay: "); Serial.print(debug_t / cnt);
-        Serial.print("\tEncoder reading: "); Serial.println(tot_angle);
+        Serial.print("\tEncoder reading: "); Serial.print(tot_angle);
+        Serial.print("\tPotentiometer reading: "); Serial.println(tot_linear);
     #endif
 }
 
